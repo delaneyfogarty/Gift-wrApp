9821a2748a0846553071bf36249c73084de29ab1
JeffAllison <itsjallison@gmail.com>
2022-04-14 10:18:58 -0700

    added comment line 50 create.js

--- create/create.js
+++ create/create.js
@@ -8,42 +8,43 @@ const zodiacDropdown = document.getElementById('zodiac-sign');
 
 window.addEventListener('load', async () => {
 
-    const months = await getMonths();
-    const zodiacSigns = await zodiacSign();
+  const months = await getMonths();
+  const zodiacSigns = await zodiacSign();
 
-    for (let month of months) {
+  for (let month of months) {
 
-        const optionEl = document.createElement('option');
-        optionEl.textContent = month.month;
-        optionEl.value = month.id;
-        selectEl.append(optionEl);
-    }
+    const optionEl = document.createElement('option');
+    optionEl.textContent = month.month;
+    optionEl.value = month.id;
+    selectEl.append(optionEl);
+  }
 
-    for (let zodiac of zodiacSigns) {
-        const zodiacOption = document.createElement('option');
-        zodiacOption.textContent = zodiac.zodiac;
-        zodiacOption.value = zodiac.id;
-        zodiacDropdown.append(zodiacOption);
-    
-    }
+  for (let zodiac of zodiacSigns) {
+    const zodiacOption = document.createElement('option');
+    zodiacOption.textContent = zodiac.zodiac;
+    zodiacOption.value = zodiac.id;
+    zodiacDropdown.append(zodiacOption);
+
+  }
 
 });
 
 form.addEventListener('submit', async (e) => {
-    e.preventDefault();
+  e.preventDefault();
 
-    const data = new FormData(form);
+  const data = new FormData(form);
 
-    const nameEl = data.get('name');
-    const monthEl = data.get('chosen-month');
-    const dayEl = data.get('day');
-    const birthYearEl = data.get('year');
-    const zodiacSignEl = data.get('zodiac-sign');
-    
+  const nameEl = data.get('name');
+  const monthEl = data.get('chosen-month');
+  const dayEl = data.get('day');
+  const birthYearEl = data.get('year');
+  const zodiacSignEl = data.get('zodiac-sign');
 
-    await createBirthday(nameEl, monthEl, dayEl, birthYearEl, zodiacSignEl);
-    
 
-    form.reset();
+  await createBirthday(nameEl, monthEl, dayEl, birthYearEl, zodiacSignEl);
+
+
+  form.reset();
 
 });
+//jeff
